ff8ede1df7b84c98ed46d17a79225b621cff8517
brennop <brennopcordeiro@gmail.com>
2020-11-19 08:37:13 -0300

    📝 Comenta o datapath/uniciclo

--- datapath.v
+++ datapath.v
@@ -3,14 +3,16 @@
 	`include "params.v"
 `endif
 
-module Datapath (
+/*
+	_Datapath do Uniciclo_
+*/
+
+module Uniciclo (
 	input clock,
 	
 	/* sinais de debug */
 	output [31:0] oPC,
 	output [31:0] oInstruction,
-	output [31:0] oRegisterRead1,
-	output [31:0] oRegisterRead2,
 	output [31:0] oDataRead,
 	output [31:0] oDataToWrite,
 		
@@ -23,30 +25,33 @@ module Datapath (
 	output dRegWrite,
 	
 	output [31:0] dAluResult,
-	output [31:0] dImmediate,
 	output [31:0] Register
-//	output [31:0] RAM [0:3]
 );
 
+// Nosso PC é um registrador de 32 bits
 reg [31:0] pc = 32'b0;
 
-wire [31:0] pcNext;
-wire [31:0] pc4;
-wire [31:0] pcImm;
+/*
+ * Fios do Datapath
+ */
+wire [31:0] pcNext;				// O que será escrito em PC
+wire [31:0] pc4;					// PC + 4
+wire [31:0] pcImm;				// PC + immediate
 
-wire [31:0] instruction;
+wire [31:0] instruction; 	// instrução que foi buscada da memória de instr.
 
-wire [31:0] registerRead1, 
-				registerRead2;
-wire [31:0] dataToWrite; 	// O que será escrito em Rd
+wire [31:0] registerRead1,	// O que foi lido de RS1 
+						registerRead2;	// O que foi lido de RS2
 
-wire [31:0] immediate;
+wire [31:0] dataToWrite; 		// O que será escrito em Rd
 
+wire [31:0] immediate;		// Imediato gerado
 wire [31:0] readData; 		// O que foi lido da mem dados
+wire [31:0] aluInputB;		// Segunda entrada da ALU (Immediate/Register)
 
-wire [31:0] aluInputB;
-
-// Sinais / Fios de Controle
+/*
+ * Sinais/Fios do Controle
+ */
 wire [1:0] OrigWriteData;	// Origem do que será escrito em rd
 wire MemRead;					// Se vamos ler da memória de dados
 wire [1:0] OrigPC;			// Origem do que será escrito em pc
@@ -61,15 +66,20 @@ wire [31:0] aluResult;
 assign pc4 = pc + 4;
 assign pcImm = pc + immediate;
 
-// Multiplexadores
+/*
+ * Multiplexadores
+ */
+
+ // O que será escrito em PC
 always @*
 	case(OrigPC)
-		PC4: 		pcNext <= pc4;
-		PCBEQ: 	pcNext <= (registerRead1 == registerRead2 ? pcImm : pc4);
-		PCIMM: 	pcNext <= pcImm;
-		default: pcNext <= pc4;
+		PC4: 			pcNext <= pc4;
+		PCBEQ: 		pcNext <= (registerRead1 == registerRead2 ? pcImm : pc4);
+		PCIMM: 		pcNext <= pcImm;
+		default: 	pcNext <= pc4;
 	endcase
 	
+ // O que será escrito em rd 
 always @*
 	case(OrigWriteData)
 		ORIG_MEM: dataToWrite <= readData;
@@ -78,6 +88,7 @@ always @*
 		ORIG_LUI: dataToWrite <= immediate;
 	endcase
 
+ // Segunda entrada da ALU
 always @*
 	case(OrigALU)
 		ORIG_REG: aluInputB <= registerRead2;
@@ -85,12 +96,17 @@ always @*
 	endcase
 
 	
-// Instanciação das Estruturas
+/*
+ * Estruturas / Módulos
+ */
+
+// Memória de Instruções
 InstructionMemory instructionMemory (
 	.address(pc), 
 	.instruction(instruction)
 );
 
+// Memória de Dados
 DataMemory dataMemory (
 	.clock(clock),
 	.write(MemWrite),
@@ -98,14 +114,15 @@ DataMemory dataMemory (
 	.iAddress(aluResult),
 	.iData(registerRead2),
 	.oData(readData)
-//	.RAM(RAM)
 );
 
+// Gerador de Imediatos
 ImmediateGenerator immGen (
 	.iInstruction(instruction),
 	.oImmediate(immediate)
 );
 
+// Banco de Registradores
 Registers registers (
 	.clock(clock), 
 	.writeRegister(RegWrite), 
@@ -118,6 +135,7 @@ Registers registers (
 	.watch(Register)
 );
 
+// Controle
 Control control (
 	.instruction(instruction),
 	.OrigWriteData(OrigWriteData),
@@ -129,6 +147,7 @@ Control control (
 	.RegWrite(RegWrite)	
 );
 
+// ALU (ULA)
 ALU alu (
 	.iControl(ALUControl),
 	.iA(registerRead1),
@@ -136,11 +155,24 @@ ALU alu (
 	.oResult(aluResult)
 );
 
-assign oInstruction = instruction;
-assign oRegisterRead1 = registerRead1;
-assign oRegisterRead2 = registerRead2;
-assign oDataRead = readData;
+/*
+  Colocamos o valor do próximo PC em
+	PC na borda de subida do clock 
+	(fim/início de um ciclo)
+*/
+always @(posedge clock)
+begin
+	pc <= pcNext;
+end
+
+/*
+ * Sinais de Debug/Saída
+ */
+
 assign oPC = pc;
+assign oInstruction = instruction;
+assign oDataRead = readData;
+assign oDataToWrite = dataToWrite;
 
 assign dOrigWriteData = OrigWriteData;
 assign dMemRead = MemRead;
@@ -152,12 +184,5 @@ assign dRegWrite = RegWrite;
 
 assign dAluResult = aluResult;
 assign dImmediate = immediate;
-
-assign oDataToWrite = dataToWrite;
-
-always @(posedge clock)
-begin
-	pc <= pcNext;
-end
 
 endmodule
